9fbef989fba3d5dc8e81c03a8ed6e151cfe31588
MmaFF <fmaa0004@student.monash.edu>
2024-10-15 16:56:04 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -145,7 +145,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     "description": "Choropleth map showing number of trips per state over time.",
                     "data": {
                         "values": tourismData,
-                        "format": { "type": "topojson", "feature": "states" }
+                        "format": { "type": "topojson", "feature": "Admin-1 states provinces" }
                     },
                     "mark": {
                         "type": "geoshape",
